0d1af8ad84888e99145839f32435e3c4fe661aa2
EC2 Default User <ec2-user@ip-172-31-40-77.ap-northeast-1.compute.internal>
2025-03-22 04:02:23 +0000

    break

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @user = current_user
@@ -14,32 +15,44 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
+    @book = Book.new(params[:id])
     if @book.save
-      flash[:notice] = "successfully"
-      redirect_to books_path
+      flash[:notice] = "You have creatad book successfully."
+      redirect_to book_path(@book.id)
     else
       render :index
     end
   end
 
   def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+        render "edit"
+    else
+        redirect_to books_path
+    end
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if @book.destroy
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
-
-end
-
 private
+
 def book_params
   params.require(:book).permit(:title, :opinion)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   root to: "homes#top"
-  get 'about', to: 'homes#about'
+  get "home/about", to: "homes#about"
   devise_for :users
   
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
-  
-  post 'books' => 'books#create'
 
 end
